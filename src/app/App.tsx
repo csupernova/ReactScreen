00e10925dda8d79a6bade208a8ef22b995f7a02e
Dmitry Kargapolov <mr.kargapolov@gmail.com>
2023-12-18 00:06:11 +0600

    add header and subheader

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import './css/App.css'
+import Header from './components/Header.tsx'
+import SubHeader from './components/subHeader.tsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <SubHeader />
+    </>
+  )
+}
+export default App
